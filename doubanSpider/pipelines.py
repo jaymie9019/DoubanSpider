e9175d5865dc75406084435a2b950c5f8232c1d5
jaymie <jaymie9019@gmail.com>
2019-01-13 15:21:08 +0800

    新增flask文档爬虫

--- doubanSpider/pipelines.py
+++ doubanSpider/pipelines.py
@@ -4,13 +4,32 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import re
+
 import redis
-r = redis.StrictRedis(host='127.0.0.1', db=0)
+import json
+from scrapy.exceptions import DropItem
 
 
 class DoubanspiderPipeline(object):
     def process_item(self, item, spider):
-        if item['score'] >= 8:
-            r.rpush('douban_movie:items', str(item))
+        if item['score'] < 8.0:
+            raise DropItem('score less than 8')
+        else:
+            self.redis.lpush('douban_movie:items', json.dumps(dict(item)))
 
         return item
+
+    def open_spider(self, spider):
+        self.redis = redis.StrictRedis(host='127.0.0.1', port=6379, db=0)
+
+
+class FlaskDocPipeline(object):
+    def process_item(self, item, spider):
+        item['text'] = re.sub(r'\s+', '', item['text'])
+        self.redis.lpush('flask_doc:items', json.dumps(dict(item)))
+        return item
+
+    def open_spider(self, spider):
+        # 连接数据库
+        self.redis = redis.StrictRedis(host='localhost', port=6379, db=0)
